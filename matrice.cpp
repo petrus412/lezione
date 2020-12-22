dcccbb40c25d4d30da8839c7a1eddc1a37f08f0f
petrus41 <sbordone.pietro@gmail.com>
2020-12-22 17:40:22 +0100

    esercizio svolto

---
+++ matrice.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	char data[100];
+	int matrice[5][6];
+	int copia[5][6];
+	ofstream outfile;
+	outfile.open("matrice.txt", ios::trunc);
+	
+	for (int i = 0; i < 5; i++)
+	{
+		for (int x = 0; x < 6; x++)
+		{
+				matrice[i][x] = x;
+				outfile << x<<" ";
+		}
+		outfile << endl;
+	}
+	outfile.close();
+	ifstream infile;
+	infile.open("matrice.txt");
+	for (int i = 0; i < 5; i++)
+	{
+		for (int x = 0; x < 6; x++)
+		{
+			infile >> copia[i][x];
+		}
+	}
+	infile.close();
+	for (int i = 0; i < 5; i++)
+	{
+		for (int x = 0; x < 6; x++)
+		{
+			cout<<copia[i][x]<<" ";
+		}
+		cout << "\n";
+	}
+}
